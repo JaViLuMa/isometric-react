1a518040b9c83446d290e7f8e415b585adcd7537
JaViLuMa <matej.gamer01@gmail.com>
2022-05-17 02:18:47 +0200

    npm package

--- src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as IsometricContainer } from "./IsometricContainer";
-export { default as Isometric } from "./Isometric";
-export { default as IsometricGrid } from "./IsometricGrid";
-export { default as IsometricPlane } from "./IsometricPlane";
-export { default as IsometricCube } from "./IsometricCube";
